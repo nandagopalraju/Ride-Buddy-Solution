427b931745df36c8b63359a7397ee8309310f9fe
nandagopalraju <nandagopalraju@gmail.com>
2022-11-21 19:46:17 +0530

    seventh commit

---
+++ RB.Core.Domain/Models/HostedRides.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RB.Core.Domain.Models
+{
+    public class HostedRides
+    {
+        public int Id { get; set; } = 0;
+        public string StartLocation { get; set; } = string.Empty;
+        public string EndLocation { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime StartTime { get; set; }
+
+        [ForeignKey("Signup")]
+        public int MemberId { get; set; }
+        public Signup Signup { get; set; }
+
+        [ForeignKey("Vehicle")]
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+    }
+}
---
+++ RB.Infrastructure.Repository/User/HostRideService.cs
@@ -0,0 +1,36 @@
+﻿using RB.Core.Application.DTO;
+using RB.Core.Application.Interface;
+using RB.Core.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RB.Infrastructure.Repository.User
+{
+    public class HostRideService : IHostRideService
+    {
+        
+        private UserDbContext _userDbContext;
+        public HostRideService(UserDbContext userDbContext)
+        {
+            _userDbContext = userDbContext;
+        }
+
+        public void HostRide(HostedRidesDTO hostedRidesDTO)
+        {
+            var hosted = new HostedRides()
+            {
+                StartLocation = hostedRidesDTO.StartLocation,
+                EndLocation = hostedRidesDTO.EndLocation,
+                StartDate = hostedRidesDTO.StartDate,
+                StartTime = hostedRidesDTO.StartTime,
+
+            };
+            _userDbContext.Add(hosted);
+            _userDbContext.SaveChanges();
+            throw new NotImplementedException();
+        }
+    }
+}
